6d587e46cb25b628c9ccabc151d18a8a0396a307
docplasma <thedocplasma@gmail.com>
2014-10-07 12:30:39 -0500

    Added Body, Engine, Velocity
    This is a draft of the structure of the Engine subsystem. The engine
    controls the iterative simulation of the solar system. Body and
    Velocity are models of celestial bodies and velocity vectors
    respectively.

---
+++ src/Engine/Body.java
@@ -0,0 +1,13 @@
+package Engine;
+
+public class Body {
+
+	float positionX;
+	float positionY;
+	float positionZ;
+	
+	float mass;
+	
+	Velocity velocity;
+	
+}
---
+++ src/Engine/Engine.java
@@ -0,0 +1,47 @@
+package Engine;
+
+public class Engine {
+	
+	//*****Global Members*****
+	Body bodies;
+	
+	//*****Main Methods*****
+	/**
+	 * This is method that is called to simulate the next step of the simulation
+	 * @author Dexter Parks
+	 * @version 1.0
+	 */
+	public void step() {
+		
+	}
+	
+	/**
+	 * This method is used to detect a collision among the bodies.
+	 * @author Dexter Parks
+	 * @version 1.0
+	 * @return True if collision occurred
+	 */
+	boolean detectCollisions() {
+		
+		//TODO Implement collision detection
+		
+		return false;
+	}
+	
+	/**
+	 * This method is used to calculate the position of the bodies during after one step
+	 * @author Charles Womble
+	 * @version 1.0
+	 * @return
+	 */
+	boolean calculatePositions() {
+		
+		//TODO Implement position calculator
+		
+		return true;
+	}
+	
+	//*****Utility Methods*****
+	
+	
+}
