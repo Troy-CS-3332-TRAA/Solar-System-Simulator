b397d36c03ca2f7e57696ac5ff78c836d20bd6ca
cwomble <womble.charles@gmail.com>
2014-11-09 13:17:07 -0600

    Wip
    still a work in progress

--- src/Gui/VisualWindow.java
+++ src/Gui/VisualWindow.java
@@ -1,5 +1,15 @@
 package Gui;
+import java.awt.*;
+import javax.swing.*;
 
-public class VisualWindow {
-
+public class VisualWindow extends JPanel
+{
+	
+	
+	public void paintComponent (Graphics g)
+	{
+		super.paintComponent(g);
+		this.setBackground(Color.black);
+	}
+	
 }
